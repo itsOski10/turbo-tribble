4cc23e2b178551094f3dc9fbf5642467d231fbd1
itsOski10 <103162378+itsOski10@users.noreply.github.com>
2022-04-14 02:13:10 -0500

    Update DataVisualizer.java
    formatted the output for DataVisualizer.java

--- src/hw/DataVisualizer.java
+++ src/hw/DataVisualizer.java
@@ -32,15 +32,20 @@ public class DataVisualizer {
 		System.out.println("Enter a title for the data:");
 		String dataTitle = sc.nextLine();
 		
+		System.out.println("You entered: " + dataTitle);
+		System.out.println();
+		
 		// 2.) Prompt the user for the headers of two columns of a table. Output the column headers.
 		
 		System.out.println("Enter the column 1 header:");
 		String columnOne = sc.nextLine();
-		
+		System.out.println("You entered: "+ columnOne);
+		System.out.println();
 		System.out.println("Enter the column 2 header:");
 		String columnTwo = sc.nextLine();
+		System.out.println("You entered: "+ columnTwo);
 		int quit = 0;
-		
+		System.out.println();
 	/*
 	 * Prompt the user for data points. Data points must be in this format: string, int. 
 	 * Store the information before the comma into a string variable and the information after the comma into an integer. 
@@ -56,28 +61,53 @@ public class DataVisualizer {
 			String dataPoint = sc.nextLine();
 			if (dataPoint.equals("-1")) {
 				quit = -1;
-			} else if (commaCount(dataPoint)!=1) {
-				System.out.println("Error: Too many commas in input.");
+			} else if (commaCount(dataPoint) == 0) {
+				System.out.println("Error: No comma in string.");
+				System.out.println();
+				
+			 	
 			} else if (!containsInt(dataPoint)) {
 				System.out.println("Error: Comma not followed by an integer.");
+				System.out.println();
+				
+			} else if(commaCount(dataPoint) > 1) {
+				System.out.println("Error: Too many commas in input.");
+				System.out.println();
+			
 			} else {
-				String tok[] = dataPoint.replace(" ", "").split(",");
+				
+				String tok[] = dataPoint.split(",");
 				strData.add(tok[0]);
 				intData.add(tok[1]);
 				System.out.println("Data string: " + tok[0] );
 				System.out.println("Data integer: " +  tok[1].trim());
+				System.out.println();
 			}
 		}
 		
 
+			System.out.println();
 			
-			
-		System.out.printf("        %s",dataTitle);
-		System.out.printf("%s         |       %s\n",columnOne,columnTwo);
+		System.out.printf("%33s\n",dataTitle);
+		System.out.printf("%-20s|%23s\n",columnOne,columnTwo);
 		System.out.println("--------------------------------------------");
+		
+		for (int i = 0; i < strData.size(); i++) {
+			System.out.printf("%-20s|%23s\n",strData.get(i),intData.get(i).trim());
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < strData.size(); i++) {
+			System.out.printf("%20s ",strData.get(i));
+			
+			for (int j = 0; j < Integer.parseInt(intData.get(i).trim()); j++) 
+				System.out.print("*");
+				System.out.println();
+			
+		}
  
-		System.out.println(strData);
-		System.out.println(intData);
+		
 	}
 
 }
